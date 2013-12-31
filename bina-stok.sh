003815da1786a4387f15a64ce5f85c915786fe1e
Muhammad Najmi Ahmad Zabidi <najmi.zabidi@gmail.com>
2014-01-01 01:12:20 +0800

    Repair Lazy governor patch files

--- bina-stok.sh
+++ bina-stok.sh
@@ -9,13 +9,13 @@ export CROSS_COMPILE=/opt/sabermod-gcc/bin/arm-eabi-
 
 export KBUILD_BUILD_USER=najmi
 export KBUILD_BUILD_HOST="ampang"
-export LOCALVERSION="-Aufa-kernel-REL-$1"
+export LOCALVERSION="-Aufa-kernel-REL-V9"
 
 #echo "Copy backup config..."
 #cp najmi-mako-config .config
 make cyanogen_mako_defconfig
-
-echo "Begin compile..."
-make -j8
+make menuconfig
+#echo "Begin compile..."
+#make -j8
 
 
