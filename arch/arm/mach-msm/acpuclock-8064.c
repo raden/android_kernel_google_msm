26ff99b305fe4ef84e91ab05f1d856247b871e95
Muhammad Najmi Ahmad Zabidi <najmi.zabidi@gmail.com>
2014-02-07 00:12:40 +0800

    Add 192MHz and 81MHz frequencies, thanks @barome

--- arch/arm/mach-msm/acpuclock-8064.c
+++ arch/arm/mach-msm/acpuclock-8064.c
@@ -155,7 +155,9 @@ static struct l2_level l2_freq_tbl[] __initdata = {
 
 static struct acpu_level tbl_slow[] __initdata = {
 #ifdef CONFIG_LOW_CPUCLOCKS
+	{ 1, {    81000, HFPLL, 2, 0x06 }, L2(0),   700000 },
 	{ 1, {   189000, HFPLL, 2, 0x0E }, L2(0),   800000 },
+  	{ 1, {   192000, PLL_8, 0, 0x00 }, L2(0),   825000 },
 	{ 1, {   270000, HFPLL, 2, 0x14 }, L2(0),   825000 },		
 	{ 1, {   384000, PLL_8, 0, 0x00 }, L2(1),   950000 },
 #else
@@ -187,7 +189,9 @@ static struct acpu_level tbl_slow[] __initdata = {
 
 static struct acpu_level tbl_nom[] __initdata = {
 #ifdef CONFIG_LOW_CPUCLOCKS
+	{ 1, {    81000, HFPLL, 2, 0x06 }, L2(0),   700000 },
 	{ 1, {   189000, HFPLL, 2, 0x0E }, L2(0),   725000 },
+  	{ 1, {   192000, PLL_8, 0, 0x00 }, L2(0),   775000 },
         { 1, {   270000, HFPLL, 2, 0x14 }, L2(0),   775000 },
 	{ 1, {   384000, PLL_8, 0, 0x00 }, L2(1),   800000 },
 #else
@@ -219,7 +223,10 @@ static struct acpu_level tbl_nom[] __initdata = {
 
 static struct acpu_level tbl_fast[] __initdata = {
 #ifdef CONFIG_LOW_CPUCLOCKS
+      
+       { 1, {    81000, HFPLL, 2, 0x06 }, L2(0),   700000 },
        { 1, {   189000, HFPLL, 2, 0x0E }, L2(0),   700000 },
+       { 1, {   192000, PLL_8, 0, 0x00 }, L2(0),   725000 },
        { 1, {   270000, HFPLL, 2, 0x14 }, L2(0),   725000 },
        { 1, {   378000, HFPLL, 2, 0x1C }, L2(0),   750000 },
        { 1, {   384000, PLL_8, 0, 0x00 }, L2(1),   750000 },
@@ -252,7 +259,9 @@ static struct acpu_level tbl_fast[] __initdata = {
 
 static struct acpu_level tbl_faster[] __initdata = {
 #ifdef CONFIG_LOW_CPUCLOCKS
+       { 1, {    81000, HFPLL, 2, 0x06 }, L2(0),   700000 },
        { 1, {   189000, HFPLL, 2, 0x0E }, L2(0),   800000 },
+       { 1, {   192000, PLL_8, 0, 0x00 }, L2(0),   725000 },      
        { 1, {   270000, HFPLL, 2, 0x14 }, L2(0),   825000 },
        { 1, {   378000, HFPLL, 2, 0x1C }, L2(0),   850000 },
 #else
