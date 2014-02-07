5dac069066388ac34b30d575ca8ac6bd7eb4fc14
Muhammad Najmi Ahmad Zabidi <najmi.zabidi@gmail.com>
2014-02-07 10:15:52 +0800

    msm: acpuclock-8064: Combine 81MHz, 189MHz and 270MHz

--- arch/arm/mach-msm/acpuclock-8064.c
+++ arch/arm/mach-msm/acpuclock-8064.c
@@ -158,7 +158,7 @@ static struct l2_level l2_freq_tbl[] __initdata = {
 static struct acpu_level tbl_slow[] __initdata = {
 #ifdef CONFIG_LOW_CPUCLOCKS
 	{ 1, {    81000, HFPLL, 2, 0x06 }, L2(0),   800000 },
-//	{ 1, {   189000, HFPLL, 2, 0x0E }, L2(0),   800000 },
+	{ 1, {   189000, HFPLL, 2, 0x0E }, L2(0),   800000 },
  // 	{ 1, {   192000, PLL_8, 0, 0x00 }, L2(0),   825000 },
 	{ 1, {   270000, HFPLL, 2, 0x14 }, L2(0),   825000 },		
 	{ 1, {   384000, PLL_8, 0, 0x00 }, L2(1),   950000 },
@@ -192,7 +192,7 @@ static struct acpu_level tbl_slow[] __initdata = {
 static struct acpu_level tbl_nom[] __initdata = {
 #ifdef CONFIG_LOW_CPUCLOCKS
 	{ 1, {    81000, HFPLL, 2, 0x06 }, L2(0),   725000 },
-//	{ 1, {   189000, HFPLL, 2, 0x0E }, L2(0),   725000 },
+	{ 1, {   189000, HFPLL, 2, 0x0E }, L2(0),   725000 },
 // 	{ 1, {   192000, PLL_8, 0, 0x00 }, L2(0),   750000 },
         { 1, {   270000, HFPLL, 2, 0x14 }, L2(0),   775000 },
 	{ 1, {   384000, PLL_8, 0, 0x00 }, L2(1),   800000 },
@@ -226,10 +226,10 @@ static struct acpu_level tbl_nom[] __initdata = {
 static struct acpu_level tbl_fast[] __initdata = {
 #ifdef CONFIG_LOW_CPUCLOCKS
        { 1, {    81000, HFPLL, 2, 0x06 }, L2(0),   700000 },
- //      { 1, {   189000, HFPLL, 2, 0x0E }, L2(0),   700000 },
+       { 1, {   189000, HFPLL, 2, 0x0E }, L2(0),   700000 },
  //      { 1, {   192000, PLL_8, 0, 0x00 }, L2(0),   725000 },
        { 1, {   270000, HFPLL, 2, 0x14 }, L2(0),   725000 },
-       { 1, {   378000, HFPLL, 2, 0x1C }, L2(0),   750000 },
+//       { 1, {   378000, HFPLL, 2, 0x1C }, L2(0),   750000 },
        { 1, {   384000, PLL_8, 0, 0x00 }, L2(1),   750000 },
 #else
        { 1, {   384000, PLL_8, 0, 0x00 }, L2(1),   850000 },
@@ -261,10 +261,10 @@ static struct acpu_level tbl_fast[] __initdata = {
 static struct acpu_level tbl_faster[] __initdata = {
 #ifdef CONFIG_LOW_CPUCLOCKS
        { 1, {    81000, HFPLL, 2, 0x06 }, L2(0),   775000 },
-  //     { 1, {   189000, HFPLL, 2, 0x0E }, L2(0),   775000 },
+       { 1, {   189000, HFPLL, 2, 0x0E }, L2(0),   775000 },
   //     { 1, {   192000, PLL_8, 0, 0x00 }, L2(0),   800000 },      
        { 1, {   270000, HFPLL, 2, 0x14 }, L2(0),   825000 },
-       { 1, {   378000, HFPLL, 2, 0x1C }, L2(0),   850000 },
+//       { 1, {   378000, HFPLL, 2, 0x1C }, L2(0),   850000 },
 #else
        { 1, {   384000, PLL_8, 0, 0x00 }, L2(0),   850000 },
 #endif
